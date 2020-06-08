4f3c320584a7007fb1e8cd6257bf16b6f71834d3
Thomas Miller <thomas.d.miller.jr@gmail.com>
2020-06-08 11:04:56 -0700

    Add color logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import { ReactComponent as Imdb } from './imdb.svg';
 import { ReactComponent as Critics } from './rt_critics.svg';
 import { ReactComponent as Audience } from './rt_audience.svg';
+import { ReactComponent as ImdbColor } from './imdb_color.svg';
+import { ReactComponent as CriticsColor } from './rt_critics_color.svg';
 
 export {
   Imdb,
   Critics,
-  Audience
+  Audience,
+  ImdbColor,
+  CriticsColor
 }
