c69829ee6c2bcf54125a4acfd260c72658613fa4
Thomas Miller <thomas.d.miller.jr@gmail.com>
2020-06-11 10:44:31 -0700

    Remove full height on gradient

--- src/components/Player.js
+++ src/components/Player.js
@@ -60,12 +60,12 @@ const ButtonWrapper = styled.div`
 
 const Gradient = styled.div`
   position: absolute;
-  top: 0;
+  top: 75%;
   left: 0;
   right: 0;
   bottom: 0;
   background: rgb(0,0,0);
-  background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 33%);
+  background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%);
 `;
 
 const Player = ({ title, posterImage, year, duration, videoSource }) => {
